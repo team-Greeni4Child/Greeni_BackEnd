e557f6a46808e5f59e66ac467619432879471810
ssongmina <thdalsdk5498@naver.com>
2025-09-27 21:07:27 +0900

    [Feature] 회원가입 DTO 작성 (#3)

--- src/main/java/com/greeni/api/members/dto/MemberRequestDTO.java
+++ src/main/java/com/greeni/api/members/dto/MemberRequestDTO.java
@@ -1,4 +1,36 @@
 package com.greeni.api.members.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+
 public class MemberRequestDTO {
+
+    @Getter
+    public static class SignUpDTO{
+
+        @NotBlank(message = "이메일은 필수 입력입니다.")
+        @Email
+        private String email;
+
+        @NotBlank(message = "비밀번호는 필수 입력입니다.")
+        @Pattern(
+                regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$",
+                message = "비밀번호는 8~15자이며, 영문자, 숫자, 특수문자를 모두 포함해야 합니다."
+        )
+        private String password;
+
+        @NotBlank(message = "인증 코드는 필수 입력입니다.")
+        private String code;
+    }
+
+    @Getter
+    public static class EmailDTO {
+
+        @NotBlank(message = "이메일은 필수 입력입니다.")
+        @Email
+        private String email;
+    }
+
 }
