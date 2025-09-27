fcf90adee00ef8736c06b21d49b2146b0dea0f84
ssongmina <thdalsdk5498@naver.com>
2025-09-27 21:07:12 +0900

    [Feature] 이메일 존재 여부 메서드 작성 (#3)

--- src/main/java/com/greeni/api/members/repository/MemberRepository.java
+++ src/main/java/com/greeni/api/members/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import com.greeni.api.members.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+
+    boolean existsByEmail(String email);
 }
