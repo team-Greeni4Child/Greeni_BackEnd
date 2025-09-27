a94bd48a7b6b115cb7c5ab3da4d9da36ada551e3
ssongmina <thdalsdk5498@naver.com>
2025-09-27 22:37:42 +0900

    [Feature] 어노테이션 추가 작성 (#3)

--- src/main/java/com/greeni/api/members/dto/MemberRequestDTO.java
+++ src/main/java/com/greeni/api/members/dto/MemberRequestDTO.java
@@ -4,14 +4,20 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class MemberRequestDTO {
 
     @Getter
+    @NoArgsConstructor
     public static class SignUpDTO{
 
         @NotBlank(message = "이메일은 필수 입력입니다.")
         @Email
+        @Pattern(
+                regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.(com|net|org|co\\.kr|go\\.kr)$",
+                message = "유효한 이메일 도메인만 입력하세요."
+        )
         private String email;
 
         @NotBlank(message = "비밀번호는 필수 입력입니다.")
@@ -26,10 +32,15 @@ public class MemberRequestDTO {
     }
 
     @Getter
+    @NoArgsConstructor
     public static class EmailDTO {
 
         @NotBlank(message = "이메일은 필수 입력입니다.")
         @Email
+        @Pattern(
+                regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.(com|net|org|co\\.kr|go\\.kr)$",
+                message = "유효한 이메일 도메인만 입력하세요."
+        )
         private String email;
     }
 
