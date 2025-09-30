8b692fc471ceacb7ddebd45269d3879fceedeb73
ssongmina <thdalsdk5498@naver.com>
2025-09-30 14:24:31 +0900

    [Feature] 이메일로 회원을 찾는 메서드 작성 (#5)

--- src/main/java/com/greeni/api/members/repository/MemberRepository.java
+++ src/main/java/com/greeni/api/members/repository/MemberRepository.java
@@ -3,7 +3,11 @@ package com.greeni.api.members.repository;
 import com.greeni.api.members.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
