0f049a4f54146bb6d45db9e0cd23f0cbc8ad264c
hcg0127 <kej431003@gmail.com>
2025-11-03 22:51:33 +0900

    [Refactor] ErrorStatus 분리 (#7)

--- src/main/java/com/greeni/api/apiPayload/status/CommonErrorStatus.java
+++ src/main/java/com/greeni/api/apiPayload/status/CommonErrorStatus.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum ErrorStatus {
+public enum CommonErrorStatus implements ErrorReason {
 
     // 가장 일반적인 응답
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
@@ -14,13 +14,12 @@ public enum ErrorStatus {
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
-    // 회원 관련 응답
-    EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4001", "이미 존재하는 메일입니다"),
-    EXPIRED_CODE(HttpStatus.BAD_REQUEST, "MEMBER4002", "이메일 인증코드가 만료되었습니다"),
-    WRONG_CODE(HttpStatus.BAD_REQUEST, "MEMBER4003", "이메일 인증코드가 올바르지 않습니다"),
-    NOT_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4004", "존재하지 않는 메일입니다"),
-    GO_TO_FINDPW(HttpStatus.BAD_REQUEST, "MEMBER4005", "비밀번호 찾기를 다시 하고 오세요"),
-    NOT_SEND_EMAIL_CODE(HttpStatus.BAD_REQUEST, "MEMBER4004", "이메일 인증코드 전송에 실패했습니다"),
+    // JSON 관련
+    JSON_PARSE_ERROR(HttpStatus.BAD_REQUEST, "JSON4001", "JSON 파싱 중 에러가 발생했습니다."),
+
+    // Argument Validation 관련
+    METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "ARGUMENT4001", "Argument Validation을 실패했습니다."),
+    TYPE_OR_FORMAT_NOT_VALID(HttpStatus.BAD_REQUEST, "ARGUMENT4002", "Argument의 타입이나 형식이 올바르지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
---
+++ src/main/java/com/greeni/api/apiPayload/status/MemberErrorStatus.java
@@ -0,0 +1,23 @@
+package com.greeni.api.apiPayload.status;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberErrorStatus implements ErrorReason {
+
+	EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4001", "이미 존재하는 메일입니다"),
+	EXPIRED_CODE(HttpStatus.BAD_REQUEST, "MEMBER4002", "이메일 인증코드가 만료되었습니다"),
+	WRONG_CODE(HttpStatus.BAD_REQUEST, "MEMBER4003", "이메일 인증코드가 올바르지 않습니다"),
+	NOT_EXIST_EMAIL(HttpStatus.BAD_REQUEST, "MEMBER4004", "존재하지 않는 메일입니다"),
+	GO_TO_FINDPW(HttpStatus.BAD_REQUEST, "MEMBER4005", "비밀번호 찾기를 다시 하고 오세요"),
+	NOT_SEND_EMAIL_CODE(HttpStatus.BAD_REQUEST, "MEMBER4004", "이메일 인증코드 전송에 실패했습니다"),
+	;
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+}
