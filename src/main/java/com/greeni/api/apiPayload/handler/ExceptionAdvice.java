b8e7b65d00fa8eb4eb3bb839bfbf32a29c3494cc
hcg0127 <kej431003@gmail.com>
2025-11-03 22:52:29 +0900

    [Refactor] 리팩터링 결과 반영 및 수정 (#7)

--- src/main/java/com/greeni/api/apiPayload/handler/ExceptionAdvice.java
+++ src/main/java/com/greeni/api/apiPayload/handler/ExceptionAdvice.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
-import com.greeni.api.apiPayload.ApiResponse;
+import com.greeni.api.apiPayload.CommonResponse;
 import com.greeni.api.apiPayload.exception.GeneralException;
-import com.greeni.api.apiPayload.status.ErrorStatus;
+import com.greeni.api.apiPayload.status.CommonErrorStatus;
+import com.greeni.api.apiPayload.status.ErrorReason;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
@@ -32,15 +34,15 @@ import java.util.Optional;
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
     // 에러 응답 생성 - String
-    private ResponseEntity<Object> handleExceptionInternal(ErrorStatus errorStatus, String message) {
-        ApiResponse<Object> body = ApiResponse.onFailure(errorStatus, message);
-        return ResponseEntity.status(errorStatus.getHttpStatus()).body(body);
+    private ResponseEntity<Object> handleExceptionInternal(ErrorReason errorReason, String message) {
+        CommonResponse<Object> body = CommonResponse.onFailure(errorReason, message);
+        return ResponseEntity.status(errorReason.getHttpStatus()).body(body);
     }
 
     // 에러 응답 생성 - Map<String, String>
-    private ResponseEntity<Object> handleExceptionInternalArgs(ErrorStatus errorStatus, Map<?,?> map) {
-        ApiResponse<Object> body = ApiResponse.onFailure(errorStatus, map);
-        return ResponseEntity.status(errorStatus.getHttpStatus()).body(body);
+    private ResponseEntity<Object> handleExceptionInternalArgs(ErrorReason errorReason, Map<?, ?> map) {
+        CommonResponse<Object> body = CommonResponse.onFailure(errorReason, map);
+        return ResponseEntity.status(errorReason.getHttpStatus()).body(body);
     }
 
     // ConstraintViolationException 핸들링
@@ -48,35 +50,37 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = ConstraintViolationException.class)
     public ResponseEntity<Object> handleConstraintsViolation(ConstraintViolationException e, WebRequest request) {
         String errorMessage = e.getConstraintViolations().stream()
-                .map(ConstraintViolation::getMessage)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("ConstraintViolationException 추출 도중 에러 발생"));
+            .map(ConstraintViolation::getMessage)
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("ConstraintViolationException 추출 도중 에러 발생"));
 
-        return handleExceptionInternal(ErrorStatus.valueOf(errorMessage),errorMessage);
+        return handleExceptionInternal(CommonErrorStatus.valueOf(errorMessage), errorMessage);
     }
 
     // MethodArgumentNotValidException 핸들링
     // @Valid로 DTO 필드의 유효성을 검사할 때 발생하는 예외
     @Override
     public ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         Map<String, String> errors = new LinkedHashMap<>();
 
         e.getBindingResult().getFieldErrors().stream()
-                .forEach(fieldError -> {
-                    String fieldName = fieldError.getField();
-                    String errorMessage = Optional.of(fieldError.getDefaultMessage()).orElse("");
-                    errors.merge(fieldName, errorMessage, (existingErrorMessage, newErrorMessage) -> existingErrorMessage + ", " + newErrorMessage);
-                });
+            .forEach(fieldError -> {
+                String fieldName = fieldError.getField();
+                String errorMessage = Optional.of(fieldError.getDefaultMessage()).orElse("");
+                errors.merge(fieldName, errorMessage,
+                    (existingErrorMessage, newErrorMessage) -> existingErrorMessage + ", " + newErrorMessage);
+            });
 
-        return handleExceptionInternalArgs(ErrorStatus._BAD_REQUEST, errors);
+        return handleExceptionInternalArgs(CommonErrorStatus.METHOD_ARGUMENT_NOT_VALID, errors);
     }
 
     // HttpMessageNotReadableException 핸들링
     // JSON을 입력받아 DTO로 변환하기 전 형식이나 타입이 유효하지 않을 떄 발생하는 예외
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
-                                                                  HttpStatusCode status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+        HttpHeaders headers,
+        HttpStatusCode status, WebRequest request) {
         Throwable cause = ex.getCause();
         String details;
 
@@ -104,22 +108,22 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
         } else {
             details = "요청 본문이 올바르지 않습니다.";
         }
-        return handleExceptionInternal(ErrorStatus._BAD_REQUEST, details);
+        return handleExceptionInternal(CommonErrorStatus.TYPE_OR_FORMAT_NOT_VALID, details);
     }
 
     // 기타 에러 핸들링
     // 따로 설정하지 않은 모든 예외
     @ExceptionHandler
-    public ResponseEntity<Object> handleOtherException(Exception e, WebRequest request) {
-        e.printStackTrace();
+    public ResponseEntity<Object> handleOtherException(Exception e, HttpServletRequest request) {
+        log.error("Unhandled exception occurred on [{} {}]", request.getMethod(), request.getRequestURI(), e);
 
-        return handleExceptionInternal(ErrorStatus._INTERNAL_SERVER_ERROR, e.getClass() + ": " + e.getMessage());
+        return handleExceptionInternal(CommonErrorStatus._INTERNAL_SERVER_ERROR, null);
     }
 
     // GeneralException 핸들링
     // throw new GeneralException(ErrorStatus.*)로 발생하는 모든 예외
     @ExceptionHandler(value = GeneralException.class)
     public ResponseEntity<Object> handleGeneralException(GeneralException e, HttpServletRequest request) {
-        return handleExceptionInternal(e.getErrorStatus(),e.getMessage());
+        return handleExceptionInternal(e.getErrorReason(), e.getMessage());
     }
 }
