c91e0f34e14e4c467dc299a7f7d186be44c4b2de
hcg0127 <kej431003@gmail.com>
2025-11-03 22:50:35 +0900

    [Feature] TimeZone 설정 - Asia/Seoul (#7)

--- src/main/java/com/greeni/api/ApiApplication.java
+++ src/main/java/com/greeni/api/ApiApplication.java
@@ -1,15 +1,20 @@
 package com.greeni.api;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableJpaAuditing
+@EnableJpaRepositories
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
