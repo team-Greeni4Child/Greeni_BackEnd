88f09937824a8243000fe36479415e8310102aad
leesiyeon <sylee3182@gmail.com>
2025-11-20 18:55:14 +0900

    [FEAT] 프로필 생성 요청 DTO에 필드 검증 추가

--- src/main/java/com/greeni/api/profiles/dto/ProfileRequestDTO.java
+++ src/main/java/com/greeni/api/profiles/dto/ProfileRequestDTO.java
@@ -1,12 +1,24 @@
 package com.greeni.api.profiles.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class ProfileRequestDTO {
 
     public record CreateProfileRequest(
             String profileImage,
+
+            @NotBlank(message = "이름은 필수 입력입니다.")
+            @Size(min = 1, max = 20, message = "이름은 1~20자 사이여야 합니다.")
+            @Schema(description = "사용자 이름", example = "그리니")
             String name,
+
+            @NotNull(message = "생년원일은 필수 입력입니다.")
+            @Schema(description = "사용자 생년월일", example = "2025-11-20")
             LocalDate birth
     ) {}
 }
