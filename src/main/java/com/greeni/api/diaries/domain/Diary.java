b8e7d123463ba1a4db32436b48b0d18d3e775839
hcg0127 <kej431003@gmail.com>
2025-12-26 14:49:16 +0900

    [FEAT] Emotion Enum 추가

--- src/main/java/com/greeni/api/diaries/domain/Diary.java
+++ src/main/java/com/greeni/api/diaries/domain/Diary.java
@@ -1,11 +1,28 @@
 package com.greeni.api.diaries.domain;
 
-import com.greeni.api.common.base.BaseEntity;
-import com.greeni.api.profiles.domain.Profile;
-import jakarta.persistence.*;
-import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+
+import com.greeni.api.common.base.BaseEntity;
+import com.greeni.api.diaries.enums.Emotion;
+import com.greeni.api.profiles.domain.Profile;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
@@ -17,20 +34,20 @@ import org.hibernate.annotations.DynamicUpdate;
 @DynamicInsert
 public class Diary extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false)
-    private String diaryImage;
+	@Column(nullable = false)
+	private String diaryImage;
 
-    @Column(nullable = false)
-    private String summary;
+	@Column(nullable = false)
+	private String summary;
 
-    @Column(nullable = false)
-    private String diaryVoice;
+	@Enumerated(EnumType.STRING)
+	private Emotion emotion;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "profile_id")
-    private Profile profile;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "profile_id")
+	private Profile profile;
 }
---
+++ src/main/java/com/greeni/api/diaries/enums/Emotion.java
@@ -0,0 +1,5 @@
+package com.greeni.api.diaries.enums;
+
+public enum Emotion {
+	HAPPY, SAD, ANGRY, ANXIETY, SURPRISED;
+}
