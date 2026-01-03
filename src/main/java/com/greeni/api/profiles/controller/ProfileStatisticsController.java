35f9e097af60574bf8181641aa205750251d0fba
leesiyeon <sylee3182@gmail.com>
2026-01-03 19:41:26 +0900

    [REFACTOR] 출석 및 일기 횟수 조회 API 컨트롤러 및 Docs 분리

---
+++ src/main/java/com/greeni/api/profiles/controller/ProfileStatisticsController.java
@@ -0,0 +1,35 @@
+package com.greeni.api.profiles.controller;
+
+import com.greeni.api.apiPayload.CommonResponse;
+import com.greeni.api.profiles.controller.docs.ProfileStatisticsControllerDocs;
+import com.greeni.api.profiles.dto.ProfileResponseDTO;
+import com.greeni.api.profiles.service.ProfileService;
+import com.greeni.api.security.jwt.userDetails.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/statistics")
+@RequiredArgsConstructor
+public class ProfileStatisticsController implements ProfileStatisticsControllerDocs {
+
+    private final ProfileService profileService;
+
+    // 출석 및 일기 횟수 조회
+    @GetMapping("/profiles/{profileId}/count")
+    public ResponseEntity<CommonResponse<ProfileResponseDTO.GetAttendanceDiaryCountResponse>> findAttendanceDiaryCount(
+            @PathVariable Long profileId,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
+    ) {
+        ProfileResponseDTO.GetAttendanceDiaryCountResponse result = profileService.getAttendanceDiaryCount(customUserDetails.getId(), profileId);
+        return new ResponseEntity<>(CommonResponse.onSuccess(result), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/greeni/api/profiles/controller/docs/ProfileStatisticsControllerDocs.java
@@ -0,0 +1,32 @@
+package com.greeni.api.profiles.controller.docs;
+
+import com.greeni.api.apiPayload.CommonResponse;
+import com.greeni.api.profiles.dto.ProfileResponseDTO;
+import com.greeni.api.security.jwt.userDetails.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Tag(name = "Statistics", description = "통계 API")
+public interface ProfileStatisticsControllerDocs {
+
+    @Operation(summary = "출석 및 일기 횟수 조회 API",
+        description = "사용자의 출석 및 일기 횟수를 조회하는 API",
+        responses = {
+             @ApiResponse(responseCode = "COMMON200", description = "요청이 성공했습니다.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProfileResponseDTO.GetProfileResponse.class))),
+             @ApiResponse(responseCode = "PROFILE4031", description = "해당 프로필에 접근할 권한이 없습니다."),
+             @ApiResponse(responseCode = "PROFILE4041", description = "존재하지 않는 프로필입니다.")
+        })
+    ResponseEntity<CommonResponse<ProfileResponseDTO.GetAttendanceDiaryCountResponse>> findAttendanceDiaryCount(
+        @Parameter(description = "조회할 프로필의 ID", required = true)
+        @PathVariable Long profileId,
+        @AuthenticationPrincipal CustomUserDetails customUserDetails
+    );
+}
