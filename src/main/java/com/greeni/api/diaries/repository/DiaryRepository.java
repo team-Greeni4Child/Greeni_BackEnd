9d566f681cb1d93e5b1666c0795c2db3cddff6d8
ssongmina <thdalsdk5498@naver.com>
2026-01-27 21:52:17 +0900

    [FEAT] 프로필 아이디, 날짜를 통해서 해당 년도,월의 일기 목록 가져오는 메서드 작성

--- src/main/java/com/greeni/api/diaries/repository/DiaryRepository.java
+++ src/main/java/com/greeni/api/diaries/repository/DiaryRepository.java
@@ -3,7 +3,12 @@ package com.greeni.api.diaries.repository;
 import com.greeni.api.diaries.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     int countByProfileId(Long profileId);
+
+    List<Diary> findByProfileIdAndCreatedAtBetweenOrderByCreatedAtAsc(Long profileId, LocalDateTime start, LocalDateTime end);
 }
