6d5d8688377e3178af715e94c705cfe1fb8d89c8
ssongmina <thdalsdk5498@naver.com>
2026-01-27 21:52:46 +0900

    [FEAT] 월별 일기 목록 조회 결과 반환용 DTO 및 컨버터 작성

--- src/main/java/com/greeni/api/diaries/converter/DiaryConverter.java
+++ src/main/java/com/greeni/api/diaries/converter/DiaryConverter.java
@@ -1,4 +1,24 @@
 package com.greeni.api.diaries.converter;
 
+import com.greeni.api.diaries.domain.Diary;
+import com.greeni.api.diaries.dto.DiaryResponseDTO;
+
+import java.util.List;
+
 public class DiaryConverter {
+
+    public static DiaryResponseDTO.MonthDiaryListDTO toMonthDiaryListDTO(Long profileId, List<Diary> diaryList){
+        List<DiaryResponseDTO.MonthDiaryDTO> result = diaryList.stream()
+                .map(diary -> DiaryResponseDTO.MonthDiaryDTO.builder()
+                        .day(diary.getCreatedAt().getDayOfMonth())
+                        .emotion(diary.getEmotion())
+                        .build()
+                )
+                .toList();
+
+        return DiaryResponseDTO.MonthDiaryListDTO.builder()
+                .profileId(profileId)
+                .diaries(result)
+                .build();
+    }
 }
--- src/main/java/com/greeni/api/diaries/dto/DiaryResponseDTO.java
+++ src/main/java/com/greeni/api/diaries/dto/DiaryResponseDTO.java
@@ -1,4 +1,30 @@
 package com.greeni.api.diaries.dto;
 
+import com.greeni.api.diaries.domain.enums.Emotion;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
 public class DiaryResponseDTO {
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    public static class MonthDiaryListDTO{
+        Long profileId;
+        List<MonthDiaryDTO> diaries;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    public static class MonthDiaryDTO{
+        Emotion emotion;
+        int day;
+    }
 }
