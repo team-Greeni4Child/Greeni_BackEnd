6bd4e92be5afadf68dea2689be2fced09800529d
ssongmina <thdalsdk5498@naver.com>
2026-01-27 21:51:43 +0900

    [FEAT] 월별 일기 목록 조회 메서드 구현

--- src/main/java/com/greeni/api/diaries/service/DiaryService.java
+++ src/main/java/com/greeni/api/diaries/service/DiaryService.java
@@ -1,4 +1,8 @@
 package com.greeni.api.diaries.service;
 
+import com.greeni.api.diaries.dto.DiaryResponseDTO;
+
 public interface DiaryService {
+
+    DiaryResponseDTO.MonthDiaryListDTO getMonthDiaryList(int year, int month, Long memberId, Long profileId);
 }
