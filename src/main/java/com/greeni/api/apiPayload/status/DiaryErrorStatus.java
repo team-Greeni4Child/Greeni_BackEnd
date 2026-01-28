17e94e28aa9affce588c53a10bc3f4e58d1d4b9b
ssongmina <thdalsdk5498@naver.com>
2026-01-28 22:45:13 +0900

    [FEAT] 일별 일기 조회시 발생하는 에러 상태 추가

--- src/main/java/com/greeni/api/apiPayload/status/DiaryErrorStatus.java
+++ src/main/java/com/greeni/api/apiPayload/status/DiaryErrorStatus.java
@@ -10,6 +10,8 @@ public enum DiaryErrorStatus implements ErrorReason {
 
     FUTURE_TIME(HttpStatus.BAD_REQUEST, "DIARY4001", "미래의 연/월입니다."),
     INVALID_MONTH(HttpStatus.BAD_REQUEST, "DIARY4002", "월은 1과 12 사이의 숫자이어야 합니다."),
+    INVALID_DAY(HttpStatus.BAD_REQUEST, "DIARY4003", "해당 월에 유효한 날짜가 아닙니다"),
+    NOT_WRITE_DIARY(HttpStatus.BAD_REQUEST, "DIARY4004", "해당 날짜에 작성한 일기가 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
