830011630d6e5a0be81a0ab93cf1356be7e97fa5
ssongmina <thdalsdk5498@naver.com>
2026-01-28 20:56:42 +0900

    [FIX] 이번달 1일부터 말일까지 조회하도록 메서드명 변경

--- src/main/java/com/greeni/api/diaries/repository/DiaryRepository.java
+++ src/main/java/com/greeni/api/diaries/repository/DiaryRepository.java
@@ -10,5 +10,6 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
     int countByProfileId(Long profileId);
 
-    List<Diary> findByProfileIdAndCreatedAtBetweenOrderByCreatedAtAsc(Long profileId, LocalDateTime start, LocalDateTime end);
+    List<Diary> findByProfileIdAndCreatedAtGreaterThanEqualAndCreatedAtLessThanOrderByCreatedAtAsc(Long profileId, LocalDateTime start, LocalDateTime end);
+
 }
