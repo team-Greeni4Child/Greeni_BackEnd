39037d704593b1a9b0d44c50154dc7657c442095
hcg0127 <kej431003@gmail.com>
2026-02-05 18:18:47 +0900

    [FEAT] ActivityType Enum에 이름 필드 추가

--- src/main/java/com/greeni/api/activities/domain/enums/ActivityType.java
+++ src/main/java/com/greeni/api/activities/domain/enums/ActivityType.java
@@ -1,5 +1,12 @@
 package com.greeni.api.activities.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ActivityType {
-	DIARY, FIVE_QUESTION, ROLE_PLAYING, BADGE
+	DIARY("일기"), FIVE_QUESTION("다섯고개"), ROLE_PLAYING("역할놀이"), BADGE("배지");
+
+	private final String name;
 }
