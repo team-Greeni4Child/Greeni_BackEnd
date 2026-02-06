cf7d517c671dc968a385c50c2d6f42a4b95588c1
hcg0127 <kej431003@gmail.com>
2026-02-06 18:22:05 +0900

    [FEAT] 배지 조건 확인 및 생성 로직 구현

--- src/main/java/com/greeni/api/badges/service/BadgeService.java
+++ src/main/java/com/greeni/api/badges/service/BadgeService.java
@@ -1,9 +1,13 @@
 package com.greeni.api.badges.service;
 
+import com.greeni.api.activities.domain.enums.ActivityType;
 import com.greeni.api.badges.dto.BadgeResponseDTO;
+import com.greeni.api.profiles.domain.Profile;
 
 public interface BadgeService {
 
-    // 배지 목록 조회
-    BadgeResponseDTO.GetBadgeListResponse getBadgeList(Long memberId, Long profileId);
+	// 배지 목록 조회
+	BadgeResponseDTO.GetBadgeListResponse getBadgeList(Long memberId, Long profileId);
+
+	void checkAndAwardBadge(Profile profile, ActivityType activityType);
 }
