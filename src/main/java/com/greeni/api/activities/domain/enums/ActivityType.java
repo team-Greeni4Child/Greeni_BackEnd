910ba800615469d905e60faabaffac835c449b26
hcg0127 <kej431003@gmail.com>
2026-02-06 18:21:05 +0900

    [FEAT] ActivityType Enum 값 추가

--- src/main/java/com/greeni/api/activities/domain/enums/ActivityType.java
+++ src/main/java/com/greeni/api/activities/domain/enums/ActivityType.java
@@ -6,7 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ActivityType {
-	DIARY("일기"), FIVE_QUESTION("다섯고개"), ROLE_PLAYING("역할놀이"), BADGE("배지");
+	ATTENDANCE("출석"),
+	DIARY("일기"),
+	FIVE_QUESTIONS("다섯고개"),
+	ROLE_PLAYING("역할놀이"),
+	BADGE("배지");
 
 	private final String name;
 }
