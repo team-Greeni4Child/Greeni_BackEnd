22a93b25c15402ea1907d09e3e4f4c81babdfeb8
ssongmina <thdalsdk5498@naver.com>
2026-03-16 23:38:00 +0900

    [FEAT] 이용 약관 관련 에러 추가

---
+++ src/main/java/com/greeni/api/apiPayload/status/TermErrorStatus.java
@@ -0,0 +1,16 @@
+package com.greeni.api.apiPayload.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum TermErrorStatus implements ErrorReason {
+    TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "TERM4001", "존재하는 이용약관이 아닙니다"),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
