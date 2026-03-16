1d4a582a8161b58372bb54214132e72e7a9c01b5
ssongmina <thdalsdk5498@naver.com>
2026-03-16 23:37:48 +0900

    [FIX] 이용약관 동의 여부 필드 추가

--- src/main/java/com/greeni/api/members/dto/MemberRequestDTO.java
+++ src/main/java/com/greeni/api/members/dto/MemberRequestDTO.java
@@ -3,6 +3,7 @@ package com.greeni.api.members.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,6 +34,15 @@ public class MemberRequestDTO {
 		@NotBlank(message = "인증 코드는 필수 입력입니다.")
 		@Schema(description = "이메일로 전송된 인증번호", example = "123456")
 		private String code;
+
+		@NotNull(message = "14세 미만 법정 대리인 동의는 필수입니다")
+		private boolean guardianConsent;
+
+		@NotNull(message = "개인정보 수집 및 이용 동의는 필수입니다")
+		private boolean personalInfoConsent;
+
+		@NotNull(message = "이용 약관 동의는 필수입니다")
+		private boolean termsAgreement;
 	}
 
 	@Getter
