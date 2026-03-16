85b0c0dafb3c67ef2bd0aba207c230d37799b631
ssongmina <thdalsdk5498@naver.com>
2026-03-16 14:12:22 +0900

    [DOCS] 컨트롤러 독스 파일 작성

--- src/main/java/com/greeni/api/diaries/controller/docs/DiaryControllerDocs.java
+++ src/main/java/com/greeni/api/diaries/controller/docs/DiaryControllerDocs.java
@@ -85,4 +85,15 @@ public interface DiaryControllerDocs {
             })
     ResponseEntity<CommonResponse<DiaryResponseDTO.CreateDiaryDTO>> makeDiary(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                                               @RequestBody @Valid DiaryRequestDTO.DiarySaveDTO request);
+
+    @Operation(summary = "일기 음성 URL 전달 API",
+            description = "일기 음성 URL을 전달하는 API",
+            responses = {
+                    @ApiResponse(responseCode = "COMMON200", description = "요청이 성공했습니다.",
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "PROFILE4041", description = "존재하지 않는 프로필입니다."),
+                    @ApiResponse(responseCode = "PROFILE4031", description = "해당 프로필에 접근할 권한이 없습니다.")
+            })
+    ResponseEntity<CommonResponse<?>> getVoice(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                                      @RequestBody @Valid DiaryRequestDTO.DiaryUrlDTO request);
 }
