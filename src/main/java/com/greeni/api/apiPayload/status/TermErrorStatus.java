1a38fcc28216bacfd86cd081e8789ae0667ad77f
ssongmina <thdalsdk5498@naver.com>
2026-03-17 16:26:58 +0900

    [FEAT] 필수 동의 항목을 동의 하지 않은 경우에 대한 에러 상황 추가

--- src/main/java/com/greeni/api/apiPayload/status/TermErrorStatus.java
+++ src/main/java/com/greeni/api/apiPayload/status/TermErrorStatus.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum TermErrorStatus implements ErrorReason {
     TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "TERM4001", "존재하는 이용약관이 아닙니다"),
+    MISSING_NECESSARY_TERM(HttpStatus.NOT_FOUND, "TERM4002", "필수 동의 항목을 동의하지 않으셨습니다"),
     ;
 
     private final HttpStatus httpStatus;
