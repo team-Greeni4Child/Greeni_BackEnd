1156bb72aae0d762222bbafa125e00d4c36c2ba0
ssongmina <thdalsdk5498@naver.com>
2026-03-29 22:12:13 +0900

    [FEAT] 일기 대화 요청 로직 작성

--- src/main/java/com/greeni/api/ai/service/AIService.java
+++ src/main/java/com/greeni/api/ai/service/AIService.java
@@ -6,9 +6,13 @@ import com.greeni.api.ai.dto.AIRequestDTO;
 import com.greeni.api.ai.dto.AIResponseDTO;
 import com.greeni.api.apiPayload.handler.GeneralException;
 import com.greeni.api.apiPayload.status.CommonErrorStatus;
+import com.greeni.api.profiles.domain.Profile;
+import com.greeni.api.profiles.service.ProfileQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Service;
@@ -23,6 +27,7 @@ import reactor.core.publisher.Mono;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -31,6 +36,8 @@ import java.util.List;
 public class AIService {
 
     private final WebClient aiWebClient;
+    private final RedisTemplate<String, String> redistemplate;
+    private final ProfileQueryService profileQueryService;
 
     public Mono<AIResponseDTO.FiveQuestionsHintResponse> fiveQuestionsHint(AIRequestDTO.FiveQuestionsHint request) {
 
@@ -195,5 +202,76 @@ public class AIService {
         }
 
         return builder;
+    }
+
+    // 일기 대화 요청 로직
+    public Mono<AIResponseDTO.DiaryResponse> diary(AIRequestDTO.DiaryRequest request, Long memberId) {
+        // 프론트에서 받은 s3 redis에 저장하는 로직
+        Profile profile = profileQueryService.findProfileAndValidate(request.profileId(), memberId);
+
+        ListOperations<String, String> ops = redistemplate.opsForList();
+        String key = "diary:voice:" + memberId + ":" + request.profileId();
+        String value = "CHILD|" + request.voiceUrl();
+        ops.rightPush(key, value);
+        if(redistemplate.getExpire(key) == -1) {
+            redistemplate.expire(key, 1, TimeUnit.HOURS);
+        }
+
+        Purpose purpose = Purpose.DIARY;
+
+        MultipartBodyBuilder builder = buildVoiceMultipart(request.voice(), purpose, null);
+
+        return aiWebClient.post()
+                .uri("/stt/transcribe")
+                .body(BodyInserters.fromMultipartData(builder.build()))
+                .retrieve()
+                .bodyToMono(AIResponseDTO.STTResponse.class)
+                .doOnError(WebClientResponseException.class, e -> {
+                    log.error("STT 에러: {}", e.getResponseBodyAsString());
+                })
+                .flatMap(sttRes -> {
+                    String recognizedText = sttRes.text();
+                    log.debug("아이가 한 말: {}", recognizedText);
+
+                    // 여기서부터 시작
+                    AIRequestDTO.DiaryInner diaryReq = new AIRequestDTO.DiaryInner(
+                            request.session_id(), request.user_text()
+                    );
+
+                    return aiWebClient.post()
+                            .uri("/diary/chat")
+                            .bodyValue(diaryReq)
+                            .retrieve()
+                            .bodyToMono(AIResponseDTO.DiaryInnerResponse.class)
+                            .doOnError(e -> log.error("Diary 에러: {}", e.getMessage()))
+                            .flatMap(diaryInnerResponse -> {
+                                AIRequestDTO.TTSRequest ttsRequest = new AIRequestDTO.TTSRequest(
+                                        purpose, diaryInnerResponse.reply(), null, null, null
+                                );
+                                return aiWebClient.post()
+                                        .uri("/tts/speak")
+                                        .bodyValue(ttsRequest)
+                                        .retrieve()
+                                        .bodyToMono(AIResponseDTO.FiveQuestionsHintInner.class)
+                                        .doOnError(WebClientResponseException.class, e ->
+                                                log.error("TTS 에러: {}", e.getResponseBodyAsString())
+                                        )
+                                        .map(ttsRes -> {
+                                            if (ttsRes.audioUrl() != null) {
+                                                String aiValue = "GREENI|" + ttsRes.audioUrl();
+                                                ops.rightPush(key, aiValue);
+                                                if (redistemplate.getExpire(key) == -1) {
+                                                    redistemplate.expire(key, 1, TimeUnit.HOURS);
+                                                }
+                                            }
+                                            return AIResponseDTO.DiaryResponse.of(
+                                                    request.session_id(),
+                                                    ttsRes.audioContent(),
+                                                    diaryInnerResponse.reply(),
+                                                    diaryInnerResponse.turn_count()
+                                            );
+                                        });
+                            });
+                });
     }
 }
